6795af77c46c82f31583a0029779316be0f6c4f1
Даниил Высоцкий <Vysotskiy.D@terempro.loc>
2017-07-05 10:03:22 +0300

    Final fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
+'use strict';
+
+$(document).ready(function () {
 	$('.owl-carousel').owlCarousel({
 		items: 1,
 		nav: true,
 		dots: false,
 		margin: 10,
-		navText: ['',''],
+		navText: ['', '']
 	});
 
 	$('.owl-carousel-feedback').owlCarousel({
@@ -12,32 +14,32 @@ $(document).ready(function(){
 		nav: true,
 		dots: false,
 		margin: 10,
-		navText: ['',''],
+		navText: ['', '']
 	});
 
 	//Плавный скролл к якорю
-	$(document).on('click', 'a', function(event){
-	    event.preventDefault();
+	$(document).on('click', 'a', function (event) {
+		event.preventDefault();
 
-	    $('html, body').animate({
-	        scrollTop: $( $.attr(this, 'href') ).offset().top - 100,
-	    }, 500);
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - 100
+		}, 500);
 	});
 
 	//Боковые кнопки input[type=number]
-	$('.up-btn').on('click', function(e){
-	  var cnt =  $(this).siblings('input').val();
-	  cnt++;
-	  $(this).siblings('input').val(cnt);
+	$('.up-btn').on('click', function (e) {
+		var cnt = $(this).siblings('input').val();
+		cnt++;
+		$(this).siblings('input').val(cnt);
 	});
 
-	$('.down-btn').on('click', function(e){
-	  var cnt =  $(this).siblings('input').val();
-	  cnt--;
-	  if (cnt < 0) {
-	  	cnt = 0;
-	  }
-	  $(this).siblings('input').val(cnt);
+	$('.down-btn').on('click', function (e) {
+		var cnt = $(this).siblings('input').val();
+		cnt--;
+		if (cnt < 0) {
+			cnt = 0;
+		}
+		$(this).siblings('input').val(cnt);
 	});
 
 	closeModal();
@@ -57,13 +59,13 @@ function showCallBackModal() {
 }
 
 function closeModal() {
-	$('.close-cross').on('click', function() {
-		$(this).closest('.modal').hide();	
-	})
+	$('.close-cross').on('click', function () {
+		$(this).closest('.modal').hide();
+	});
 }
 
 function toggleNavBtn() {
-	if($(window).width() < 691) {
+	if ($(window).width() < 691) {
 		$('.navbar-toggle').show();
 		$('.header__nav').hide();
 	} else {
@@ -71,18 +73,17 @@ function toggleNavBtn() {
 		$('.header__nav').show();
 	}
 
-	$('.navbar-toggle').on('click', function(){
+	$('.navbar-toggle').on('click', function () {
 		$('.header__nav').toggle();
-	})
+	});
 
-	$(window).on('resize', function(e) {
-		if($(this).width() < 691) {
+	$(window).on('resize', function (e) {
+		if ($(this).width() < 691) {
 			$('.navbar-toggle').show();
 			$('.header__nav').hide();
 		} else {
 			$('.navbar-toggle').hide();
 			$('.header__nav').show();
 		}
-	})
+	});
 }
-
